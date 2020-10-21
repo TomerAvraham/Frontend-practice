b0ec86a0a48139c8a0af0be6de9616ca78bc6635
Tomer Avraham <avrahamtomer710@gmail.com>
2020-10-21 14:02:13 +0300

    inital commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/HomePage/Home";
+import Footer from "./components/pages/Footer/Footer";
+import Services from "./components/pages/Services/Services";
+import Products from "./components/pages/Products/Products";
+import SignUp from './components/pages/SignUp/SignUp'
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" exact component={Services} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/sing-up" exact component={SignUp} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
